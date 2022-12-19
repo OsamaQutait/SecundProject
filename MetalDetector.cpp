425925002f2efd179f5b9c36b548988a78521420
Osama Qutait <oaqutait@gmail.com>
2022-12-19 12:40:15 +0200

    fork

--- MetalDetector.cpp
+++ MetalDetector.cpp
@@ -3,6 +3,25 @@
 int generate_waiting_time(int lower, int upper);
 
 int main(int argc, char *argv[]) {
+    unordered_map<string, int> data;
+    // read file
+    string line, word, num;
+    fstream data_file;
+    data_file.open("inputData.txt", ios::in);
+    if (!data_file){
+        perror("File not created!");
+        exit(-1);
+    } else {
+        while (getline(data_file, line)){
+            stringstream ss(line);
+            ss >> word;
+            ss >> num;
+            data[word] = stoi(num); // casting to integer
+        }
+        data_file.close();
+    }
+    // end reading
+
 
 
     return 0;
--- RollingGates.cpp
+++ RollingGates.cpp
@@ -3,6 +3,24 @@
 int generate_waiting_time(int lower, int upper);
 
 int main(int argc, char *argv[]) {
+    unordered_map<string, int> data;
+    // read file
+    string line, word, num;
+    fstream data_file;
+    data_file.open("inputData.txt", ios::in);
+    if (!data_file){
+        perror("File not created!");
+        exit(-1);
+    } else {
+        while (getline(data_file, line)){
+            stringstream ss(line);
+            ss >> word;
+            ss >> num;
+            data[word] = stoi(num); // casting to integer
+        }
+        data_file.close();
+    }
+    // end reading
 //    sleep(15); // for real code , for test I will put it 2 ,to minimize waiting time
     sleep(2);
     int wait = generate_waiting_time(1, 10);
