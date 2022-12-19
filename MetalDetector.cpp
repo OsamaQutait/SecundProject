f6e679869caf6438e5b08fa2f1132b027e409e88
Osama Qutait <oaqutait@gmail.com>
2022-12-19 09:54:24 +0200

    fork

---
+++ MetalDetector.cpp
@@ -0,0 +1,20 @@
+#include "local.h"
+
+int generate_waiting_time(int lower, int upper);
+
+int main(int argc, char *argv[]) {
+
+
+    return 0;
+}
+
+int generate_waiting_time(int lower, int upper){
+    int s, numb;
+    FILE *f;
+    f = fopen("/dev/urandom", "rb");
+    fread(&s, sizeof(int), 1, f);
+    fclose(f);
+    srand(s);
+    int num = (rand() % (upper - lower + 1)) + lower;
+    return num;
+}
